4982465b3e886cfc90d2846fb4d56e1cb00bd875
Shyamanth-3 <shyamanthnadella.03082007@gmail.com>
2026-06-05 16:20:56 +0530

    feat: add Layout component with scroll-to-top functionality and analytics integration

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import { Outlet, useLocation } from 'react-router-dom';
@@ -7,6 +7,10 @@ import { Analytics } from '@vercel/analytics/react';
 export default function Layout() {
   const location = useLocation();
   const isHome = location.pathname === '/';
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
 
   return (
     <div className="flex flex-col min-h-screen">
